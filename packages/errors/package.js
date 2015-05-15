2111005b59c9165cffe8dd2ed789e8dd75932a18
Diego de Souza <diegotrus7@gmail.com>
2015-05-15 17:51:01 -0400

    Added tests to the package.

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -2,11 +2,18 @@ Package.describe({
   summary: "A pattern to display application errors to the user"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
 
-  api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
 
   if (api.export)
     api.export('Errors');
+});
+
+Package.onTest(function(api) {
+  api.use('errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.addFiles('errors_tests.js', 'client');
 });
