3acc0b7c67be2e67865e158fb4029b11196a7e19
Diego de Souza <diegotrus7@gmail.com>
2015-05-14 22:42:55 -0400

    Demonstrate the order that posts appear using a sleep

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -12,7 +12,11 @@ Template.postSubmit.events({
       if (error)
         return alert(error.reason);
 
-      Router.go('postPage', {_id: result._id});
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
     });
+
+    Router.go('postsList');
   }
 });
